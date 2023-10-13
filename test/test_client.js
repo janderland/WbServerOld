3219a8ddcbb6b1bfe1eb2991cdfb278414465d9e
Jon Anderson <jon.david.anderson@gmail.com>
2023-10-12 23:34:54 -0700

    Improved NullClient tests. Broken Client tests.

--- test/test_client.js
+++ test/test_client.js
@@ -1,11 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.204.0/assert/mod.ts";
-import { delay } from "https://deno.land/std@0.204.0/async/delay.ts";
-import { NullClient } from "../client.js";
+import { Client, NullClient } from "../client.js";
+import * as msg from "../messages.js";
 
 Deno.test("NullClient", async (t) => {
   const client = new NullClient();
 
-  await t.step("namePlease", () => {
+  await t.step("name", () => {
     let name = "";
     client.onName = (n) => {
       name = n;
@@ -14,17 +14,44 @@ Deno.test("NullClient", async (t) => {
     assertEquals(name, "NULL");
   });
 
-  await t.step("countDown", async () => {
-    let clicked = false;
-    client.onClick = () => {
-      clicked = true;
-    };
+  await t.step("clicking", async () => {
+    let clicked = new Promise((resolve) => {
+      client.onClick = resolve;
+    });
+
     client.countDown(0);
-    await delay(750);
-    assertEquals(clicked, true);
+    await clicked;
 
     // Call gameOver so the
     // interval is cleared.
     client.gameOver(true);
   });
+});
+
+Deno.test("Client", async (t) => {
+  let client;
+  let conn;
+
+  Deno.serve((req) => {
+    if (req.headers.get("upgrade") !== "websocket") {
+      return new Response(null, { status: 501 });
+    }
+    const { socket, response } = Deno.upgradeWebSocket(req);
+    client = new Client(socket);
+    return response;
+  });
+
+  conn = new WebSocket("ws://localhost:8000");
+  await new Promise((resolve) => {
+    conn.addEventListener("open", resolve);
+  });
+
+  let name = new Promise((resolve) => {
+    client.onName = resolve;
+  });
+
+  await conn.send(JSON.stringify(msg.Name("test")));
+  assertEquals(await name, "test");
+
+  conn.close();
 });
